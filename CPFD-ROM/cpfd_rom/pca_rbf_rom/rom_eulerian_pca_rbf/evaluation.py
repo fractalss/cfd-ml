9d821f256df6d68d4971e870246f3e9af1d976eb
Saurav Mitra <saurav.mitra@cpfd-software.com>
2025-07-30 13:48:22 -0500

    remove txt based rom from binary rom

--- CPFD-ROM/cpfd_rom/pca_rbf_rom/rom_eulerian_pca_rbf/evaluation.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-from sklearn.metrics import root_mean_squared_error
-
-def compute_rmse(rom_df, cfd_df, field_variable):
-    merged_df = pd.merge(rom_df, cfd_df, on=['x', 'y', 'z'], suffixes=('_ROM', '_CFD'))
-    rmse = root_mean_squared_error(merged_df[f'{field_variable}_CFD'], merged_df[f'{field_variable}_ROM'])
-    return rmse, merged_df
--- CPFD-ROM/cpfd_rom/rom_cli.py
+++
@@ -1,5 +0,0 @@
-# rom_cli.py
-from cpfd_rom.main_entry import main
-
-if __name__ == "__main__":
-    main()
